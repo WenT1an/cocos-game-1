1ab7b41ef7cb02be6d675e59e5981d199adc6e2b
WenT1an <1445139581@qq.com>
2026-03-22 17:32:48 +0800

    Update index.bb01b.js

--- index.bb01b.js
+++ index.bb01b.js
@@ -23,14 +23,6 @@ System.register(["./application.75bfb.js"], function (_export, _context) {
       })["catch"](function (err) {
         console.error(err);
       });
-
-      System.import('cc').then((engine) => {
-    // engine 初始化后可以动态 import _virtual_cc 模块
-    return System.import('virtual:_virtual_cc-Bp5iGE8z');
-}).then((_virtualModule) => {
-    // _virtualModule 已经加载完成
-    application.init(engine).then(() => application.start());
-}).catch(console.error);
     }
   };
 });
